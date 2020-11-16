7b1f62ad3b90990f038aed667033dda91093c91f
corneliajohnson <corneliaj91@gmail.com>
2020-11-16 11:19:06 -0500

    give the user 4 tries

--- Program.cs
+++ Program.cs
@@ -6,10 +6,20 @@ namespace GuessingGame
   {
     static void Main(string[] args)
     {
+      AskUser();
+    }
+
+    static void AskUser()
+    {
       int sercetNumber = 42;
-      Console.Write("Guess a number a number: ");
-      string userGuess = Console.ReadLine();
-      CheckGuess(sercetNumber, userGuess);
+      int tries = 0;
+      while (tries < 4)
+      {
+        tries++;
+        Console.Write("Guess a number a number: ");
+        string userGuess = Console.ReadLine();
+        CheckGuess(sercetNumber, userGuess);
+      }
     }
 
     static void CheckGuess(int secrectNum, string userNum)
@@ -18,6 +28,7 @@ namespace GuessingGame
       if (userNumInt == secrectNum)
       {
         Console.WriteLine("You Guessed RIGHT");
+        Environment.Exit(-1);
       }
       else
       {
